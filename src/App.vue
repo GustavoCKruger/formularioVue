01cee1e2f2d1a26b5e041f965f2937d3b7ff3cc1
GustavoCKruger <gustavo.kruger1601@gmail.com>
2024-03-22 16:20:49 -0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,11 @@ const usuario = ref({
 function enviar() {
   alert(`Seus dados são: \nNome: ${usuario.value.nome} \nIdade: ${usuario.value.idade} \nEmail: ${usuario.value.email}`)
 
-  if (usuario.value.idade < 18 || usuario.value.idade > 60) {
-    alert(`Arrume sua idade`)
+  if (usuario.value.idade < 18) {
+    alert(`Apenas maiores de idade 🚫18`);
+  }
+  else if (usuario.value.idade > 60) {
+    alert(`Apenas menores da era paleozoica`);
   }
 }
 
@@ -22,21 +25,38 @@ function enviar() {
   <form>
     <div class="nome">
       <label for="nome">Nome:</label>
-      <input type="text" name="nome" id="nome" v-model="usuario.nome" placeholder="Insira seu nome aqui">
+      <br>
+      <input class="placeholder" minlength="3" maxlength="20" type="text" name="nome" id="nome" v-model="usuario.nome"
+        placeholder=" Insira seu nome aqui">
     </div>
 
     <div class="idade">
       <label for="idade">Idade:</label>
-      <input type="number" name="idade" id="idade" v-model="usuario.idade">
+      <br>
+      <input class="placeholder" type="number" name="idade" id="idade" v-model="usuario.idade"
+        placeholder=" Insira sua idade aqui">
     </div>
 
     <div class="email">
       <label for="email">Email:</label>
-      <input type="email" name="email" id="email" v-model="usuario.email" placeholder="Insira seu email aqui">
+      <br>
+      <input class="placeholder" type="email" name="email" id="email" v-model="usuario.email"
+        placeholder=" Insira seu email aqui">
     </div>
-
+    <br>
     <button type="submit" @click="enviar()">Enviar</button>
   </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+form {
+  background-color: burlywood;
+  max-height: 100vh;
+  max-width: 100vh;
+  border: 20px black;
+}
+
+.placeholder {
+  width: 30vh;
+}
+</style>
